714e9835155a6dc5247096eec6d0ae6a0b74d17a
Celine Wan <mwanm074@uottawa.ca>
2021-01-10 17:58:53 -0500

    Added a few commands for the bot such as a magic 8 ball and the server info, also regenerated token but won't update the token on code

--- bot.py
+++ bot.py
@@ -16,5 +16,41 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} has left a server.')
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx,*,question):
+    responses = ['It is certain.',
+                 'It is decidedly so.',
+                 'Without a doubt.',
+                 'Yes – definitely.',
+                 'You may rely on it.',
+                 'As I see it, yes.'
+                 'Most likely.',
+                 'Outlook good.',
+                 'Yes.',
+                 'Signs point to yes.',
+                 'Reply hazy, try again.',
+                 'Ask again later.',
+                 'Better not tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 'Don\'t count on it.',
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Very doubtful.']
+    
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+@client.command()
+async def infoserver(ctx):
+    await ctx.send('The purpose of this server is for Celine who\'s trying to make a discord bot')
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
 
 client.run('Nzk3NTE3NDQ3ODQ5NzA1NDgy.X_noAA.sYfzP1j3xjGwHct9mN9D8LrvSRc')
